0c1651a38cbeca41590a2cf8a42224bbc4572e9d
alberto <albertoromanhol@gmail.com>
2022-05-15 14:25:58 -0300

    Update EmailTest.cs

--- src/MecOrb.Tests/ValueObjects/EmailTest.cs
+++ src/MecOrb.Tests/ValueObjects/EmailTest.cs
@@ -22,8 +22,8 @@ namespace MecOrb.Tests.ValueObjects
         }
 
         [Theory]
-        [InlineData("joao@localiza.com")]
-        [InlineData("joao.souza@localiza.com")]
+        [InlineData("joao@email.com")]
+        [InlineData("joao.souza@email.com")]
         [InlineData("joao.silva@microsoft.com.br")]
         public void CriarEmail_EmailValido_Test(string endereco)
         {
